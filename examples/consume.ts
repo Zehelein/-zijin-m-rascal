cd0c71542d85e47aef2daea970741de913c2b8af
Frank Zehelein <zehelein@axinom.com>
2020-07-01 22:21:54 +0200

    example cleanup

--- examples/consume.ts
+++ examples/consume.ts
@@ -1,4 +1,4 @@
-import { Broker, Consumer, Message } from "..";
+import { Broker, Consumer, Message } from "../dist";
 import config from "./config";
 
 (async () => {
@@ -6,31 +6,21 @@ import config from "./config";
     public readonly name = "order.save"; // set subscribe name or use new OrderSaveConsumer("order.save")
 
     async onMessage(content: any, message: Message) {
-      const forwardedCount =
-        message.properties.headers.rascal?.recovery?.order?.save?.service_b
-          ?.forwarded;
-
-      if (forwardedCount > 0) {
-        console.log(
-          "Already forwarded number of times: ",
-          forwardedCount,
-          // JSON.stringify(message, null, 1),
-          content
-        );
-      }
-
       // your code
+      const count =
+        message.properties.headers.rascal?.recovery?.order?.save?.service_b
+          ?.forwarded || 0;
+
+      // provoke random crashes to test redeliveries
       if ((content as string)?.indexOf("1") >= 0 && Math.random() > 0.5) {
-        throw new Error("some message error");
+        console.log(`crashing for '${content}' (had ${count} redeliveries)`);
+        throw new Error("some processing error");
       }
 
-      if (forwardedCount > 0) {
-        console.log(
-          `content: ${content} solved after ${forwardedCount} attempts.`
-        );
-      } else {
-        console.log("content:" + content);
-      }
+      console.log(
+        `'${content}' processed`,
+        count > 0 ? `after ${count} redeliveries` : ""
+      );
       // no need to call ackOrNack if your code success, Consumer will do this for you
     }
   }
